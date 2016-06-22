a00dacf10341d9f13a615e10b2ae71220dea70a2
Dewey Sasser <dewey@sasser.com>
2016-06-22 15:55:55 -0400

    Ensure /etc/proxy/ exists before using it

--- events.sh
+++ events.sh
@@ -3,10 +3,11 @@
 # Purpose: consume docker events and do interesting things
 
 # Run once initially
+mkdir -p /etc/proxy
 `dirname $0`/build-proxy-config.py
 
 docker events -f type=container -f event=die -f event=start | while read time ID LINE ; do
     echo "id $ID: $LINE"
     `dirname $0`/build-proxy-config.py
-    sh /etc/proxy/notify.sh
+    test -e /etc/proxy/notify.sh && sh /etc/proxy/notify.sh
 done
